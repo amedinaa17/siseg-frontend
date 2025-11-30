9403619bec4ba2d17e78c9c35c2f60a29f7d14b9
saulalv12 <RockoCam11>
2025-11-30 12:08:30 -0600

    Se agrega el api de google maps para el funcionamiento correcto del mapa en el apk

--- app.config.ts
+++ app.config.ts
@@ -6,14 +6,19 @@ export default {
     orientation: "portrait",
     icon: "./activos/imagenes/siseg.png",
     scheme: "SISEG",
+
     extra: {
       API_URL: process.env.API_URL,
+      GOOGLE_MAPS_API_KEY:
+        process.env.GOOGLE_MAPS_API_KEY || "AIzaSyAN9dlyun_A3YnM9gR3NbMob6y6IPntkNA",
       eas: {
         projectId: "733f616e-e525-4a99-921e-38393a115507"
       }
     },
+
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
+
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.tt2025b020.siseg",
@@ -21,20 +26,30 @@ export default {
         ITSAppUsesNonExemptEncryption: false
       }
     },
+
     android: {
       package: "com.tt2025b020.tuapp",
       adaptiveIcon: {
         foregroundImage: "./activos/imagenes/siseg.png",
         backgroundColor: "#ffffff"
       },
-      edgeToEdgeEnabled: true
+      edgeToEdgeEnabled: true,
+      config: {
+        googleMaps: {
+          apiKey:
+            process.env.GOOGLE_MAPS_API_KEY ||
+            "AIzaSyAN9dlyun_A3YnM9gR3NbMob6y6IPntkNA",
+        },
+      },
     },
+
     web: {
       bundler: "metro",
       output: "static",
       favicon: "./activos/imagenes/favicon.png",
       name: "SISEG"
     },
+
     plugins: [
       "expo-router",
       [
@@ -49,6 +64,7 @@ export default {
       "expo-secure-store",
       "expo-font"
     ],
+
     experiments: {
       typedRoutes: true
     }
